53eb699ac89f2c921a50334b22a1a95f4819d239
Ondrej Lichtner <ondrej.lichtner@gmail.com>
2014-04-06 19:17:02 +0200

    mkdir net-device/ create NetDevice interface class
    The interface classes defining the NetDevice hierarchy will be defined
    in the net-device directory. The NetDevice class is the root class
    defining the most common functions. From there all the other classes
    will be inherited.
    
    The implementation of the classes will be in a different directory.

---
+++ net-device/eth-device.h
@@ -0,0 +1,9 @@
+#ifndef __ETH_DEVICE_H__
+#define __ETH_DEVICE_H__
+
+#include "net-device.h"
+
+class EthDevice: private NetDevice{
+}
+
+#endif
---
+++ net-device/net-device.h
@@ -0,0 +1,23 @@
+#ifndef __NETDEVICE_H__
+#define __NETDEVICE_H__
+
+#include <string>
+
+class NetDevice{
+    public:
+        virtual int get_if_id() = 0;
+
+        virtual std::string get_name() = 0;
+        virtual bool set_name(std::string name) = 0;
+
+        virtual std::vector<char> get_hw_address() = 0;
+        virtual bool set_hw_address(std::vector<char> hw_addr) = 0;
+
+        virtual std::vector<IpAddr *> get_ip_addr_list() = 0;
+        virtual bool add_ip_addr(IpAddr &addr) = 0;
+        virtual bool del_ip_addr(IpAddr &addr) = 0;
+
+        virtual int get_device_type() = 0;
+}
+
+#endif
