770149c2c1508ac18eabb606cd5c091a88f03d48
Ondrej Lichtner <ondrej.lichtner@gmail.com>
2014-04-06 18:45:41 +0200

    template for the LibNCFG class
    This commit renames the lib-ncfg.{cpp, h} files to libncfg.{cpp, h} and
    declares the LibNCFG class that will be used to access the rest of the
    functionality of the library.

---
+++ libncfg.cpp
@@ -0,0 +1,59 @@
+#include "libncfg.h"
+
+LibNCFG::LibNCFG()
+{
+    //init - find and load dynamically loaded modules
+}
+
+std::vector<NetDevice *> LibNCFG::get_devices()
+{
+    //returns a vector of all network devices currently on the system
+    return this->devices;
+}
+
+NetDevice * LibNCFG::get_device(std::string name)
+{
+    //returns the network device with the specified name
+}
+
+NetDevice * LibNCFG::get_device(int if_id)
+{
+    //returns the network device with the specified interface id
+}
+
+NetDevice * LibNCFG::create_soft_device(int type)
+{
+    //creates a device of the specified type, automatically generates a name
+}
+
+NetDevice * LibNCFG::create_soft_device(int type, std::string name)
+{
+    //creates a device of the specified type, with the specified name
+}
+
+void LibNCFG::destroy_soft_device(NetDevice *device)
+{
+    //destroys the device
+}
+
+void LibNCFG::destroy_soft_device(std::string name)
+{
+    //destroys the device
+}
+
+void LibNCFG::destroy_soft_device(int if_id)
+{
+    //destroys the device
+}
+
+int LibNCFG::define_callback(void *(*func)(int event, NetDevice *source),
+                             int event)
+{
+    //registers a callback function that can be called when an event is catched
+    //the events are only detected when the method spin_callbacks is called
+}
+
+void LibNCFG::spin_callbacks()
+{
+    //updates all devices and detects events, will call a callback function if defined
+}
---
+++ libncfg.h
@@ -0,0 +1,29 @@
+#ifndef __LIBNCFG_H__
+#define __LIBNCFG_H__
+
+#include <vector>
+#include <string>
+
+class LibNCFG{
+    private:
+        std::vector<NetDevice *> devices;
+
+    public:
+        LibNCFG();
+        std::vector<NetDevice *> get_devices();
+        NetDevice *get_device(std::string name);
+        NetDevice *get_device(int if_id);
+
+        NetDevice *create_soft_device(int type);
+        NetDevice *create_soft_device(int type, std::string name);
+        void destroy_soft_device(NetDevice *device);
+        void destroy_soft_device(std::string name);
+        void destroy_soft_device(int if_id);
+
+        int define_callback(void *(*func)(int event, NetDevice *source), int event);
+        void spin_callbacks();
+
+        //add tables
+}
+
+#endif
