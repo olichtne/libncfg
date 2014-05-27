6e1a4d15bdf2b3ad1d3f5cf75e33c2f11369ff9b
Ondrej Lichtner <ondrej.lichtner@gmail.com>
2014-05-27 16:14:30 +0200

    add GPLv3 licensing

--- include/libncfg/common.h
+++ include/libncfg/common.h
@@ -1,3 +1,21 @@
+/* Copyright (C) 2014 Ondrej Lichtner <ondrej.lichtner@gmail.com>
+ *
+ * This file is part of the LibNCFG library.
+ *
+ * The LibNCFG library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The LibNCFG library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the LibNCFG library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __LIBNCFG_COMMON__
 #define __LIBNCFG_COMMON__
 
